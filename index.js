fc93958738129b2930e0d0ddd575e7e98c935e22
Pulse04 <190657331+Pulse04@users.noreply.github.com>
2025-01-16 00:36:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Map-view')));
 
 // Serve the index.html file for the root path
 app.get('/', (req, res) => {
